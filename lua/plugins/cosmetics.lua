f1e873543a9b0ec4f13df955db50b3eaefe2dddd
Andrea Rossi <a.rossi189@campus.unimib.it>
2024-10-09 05:56:46 +0200

    updated background

--- lua/plugins/cosmetics.lua
+++ lua/plugins/cosmetics.lua
@@ -2,7 +2,14 @@ return {
     {
         "ellisonleao/gruvbox.nvim",
         priority = 1000,
+        contrast = "hard",
         config = function()
+            require("gruvbox").setup({
+                contrast = "hard",
+                palette_overrides = {
+                    dark0_hard = "#000000",
+                },
+            })
             vim.cmd("colorscheme gruvbox")
         end,
     },
