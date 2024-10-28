1b1c18beb30e46f768f1e0c1b48dfc9c62c63c59
Andrea Rossi <andrea.rossi03@pm.me>
2024-10-28 17:55:39 +0100

    reverted background

--- lua/plugins/cosmetics.lua
+++ lua/plugins/cosmetics.lua
@@ -6,9 +6,12 @@ return {
         config = function()
             require("gruvbox").setup({
                 contrast = "hard",
+                --[[                
                 palette_overrides = {
                     dark0_hard = "#000000",
                 },
+                ]]
+                --
             })
             vim.cmd("colorscheme gruvbox")
         end,
